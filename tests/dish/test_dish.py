8a44642cf594a22c197563502a84834ad68b2cfe
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-06-19 10:02:52 -0300

    feat: req2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,38 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    prato1 = Dish("Rabanada", 8.5)
+    prato2 = Dish("Brioche", 6.0)
+
+    prato1.add_ingredient_dependency(Ingredient("ovos"), 4)
+
+    with pytest.raises(
+        TypeError, match="O preço do prato deve ser um número real."
+    ):
+        Dish("Rabanada", "precoteste")
+
+    with pytest.raises(
+        ValueError, match="O preço do prato deve ser maior que zero."
+    ):
+        Dish("Rabanada", 0)
+
+    assert prato1.nome == "Rabanada"
+
+    assert hash(prato1) == hash(prato1)
+    assert hash(prato1) != hash(prato2)
+
+    assert prato1 == prato1
+
+    assert repr(prato1) == "Dish('Rabanada', R$8.50)"
+
+    assert prato1.receita.get(Ingredient("ovos")) == 4
+
+    assert prato1.obter_restricoes() == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.EGG,
+    }
+
+    assert prato1.obter_ingredientes() == {Ingredient("ovos")}
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -2,14 +2,14 @@ from src.models.ingredient import Ingredient, Restriction
 
 
 def test_ingredient():
-    ingredient1 = Ingredient('farinha')
-    ingredient2 = Ingredient('bacon')
-    ingredient3 = Ingredient('farinha')
+    ingredient1 = Ingredient("farinha")
+    ingredient2 = Ingredient("bacon")
+    ingredient3 = Ingredient("farinha")
 
     assert ingredient1.__hash__() == ingredient3.__hash__()
     assert ingredient1.__hash__() != ingredient2.__hash__()
     assert ingredient1.__eq__(ingredient3)
     assert ingredient1.__repr__() == "Ingredient('farinha')"
-    assert ingredient1.name == 'farinha'
-    assert ingredient2.name != 'veganbacon'
+    assert ingredient1.name == "farinha"
+    assert ingredient2.name != "veganbacon"
     assert ingredient1.restrictions == {Restriction.GLUTEN}
